fb0faa7ad6e2d1d7fdd1cc5d68010562cef335a9
CECO <cvetomir97@abv.bg>
2023-05-01 22:20:46 +0300

    Configuring gRPC interface

---
+++ WebShopDemo/WebShopDemo.Grpc/Services/ProductGrpcService.cs
@@ -0,0 +1,26 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using WebShopDemo.Core.Contracts;
+using System.Linq;
+
+namespace WebShopDemo.Grpc.Services
+{
+    public class ProductGrpcService : Product.ProductBase
+    {
+        private readonly IProductService productService;
+        public ProductGrpcService(IProductService _productService)
+        {
+            this.productService = _productService;
+        }
+        public override Task<ProductList> GetAll(Empty request, ServerCallContext context)
+        {
+           ProductList result = new ProductList();
+            var products = this.productService.GetAll();
+
+            result.Items.AddRange(products.Select(p => new ProductItem()
+            {
+
+            }));
+        }
+    }
+}
