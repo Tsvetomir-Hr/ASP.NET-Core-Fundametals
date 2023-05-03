302b8f29849244ff528a07b0132b8359c100c8d7
CECO <cvetomir97@abv.bg>
2023-05-03 23:51:52 +0300

    configuring PostgreSQL

--- WebShopDemo/WebShopDemo/Program.cs
+++ WebShopDemo/WebShopDemo/Program.cs
@@ -8,9 +8,10 @@ using WebShopDemo.Core.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
