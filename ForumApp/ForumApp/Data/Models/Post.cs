68daec6bf568b2a4a454d20dadb541a1cd309269
CECO <cvetomir97@abv.bg>
2023-05-11 23:59:03 +0300

    Creating the entity of the demo project

---
+++ ForumApp/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Data.Models
+{
+    [Comment("Published posts")]
+    public class Post
+    {
+        [Key]
+        [Comment("Post identifier")]
+        public int Id { get; set; }
+
+        [Comment("Post title")]
+        [Required]
+        public string Title { get; set; }
+
+        [Comment("Post content")]
+        [Required]
+        public string Content { get; set; }
+    }
+}
