dfa2ed9d9fc60c9ddf7cb1631fadbef67c1705c1
CECO <cvetomir97@abv.bg>
2023-05-12 00:21:27 +0300

    Created dbContext

---
+++ ForumApp/ForumApp/Data/ForumDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ForumDbContext : DbContext
+    {
+
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
