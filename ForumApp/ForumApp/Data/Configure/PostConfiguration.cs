d97aafcf4df807ca578190c5c38050063d9134ad
CECO <cvetomir97@abv.bg>
2023-05-12 23:16:11 +0300

    Demo - seed configuration and use.

--- ForumApp/ForumApp/Data/Configure/PostConfiguration.cs
+++ ForumApp/ForumApp/Data/Configure/PostConfiguration.cs
@@ -17,9 +17,24 @@ namespace ForumApp.Data.Configure
         {
             return new List<Post>()
             {
-                new Post(){ },
-                new Post(){ },
-                new Post(){ }
+                new Post()
+                {
+                    Id = 1,
+                    Title = "My first post",
+                    Content = "My first post will be about performing CRUD operations in MVC.It is so much fun."
+                },
+                new Post()
+                {
+                    Id = 2,
+                    Title = "My second post",
+                    Content = "This is my second post. CRUD operations in MVC are getting more and more interesting."
+                },
+                new Post()
+                {
+                    Id = 3,
+                    Title = "My third post",
+                    Content = "Hello there! I am getting better and better in CRUD operations in MVC."
+                }
 
             };
         }
--- ForumApp/ForumApp/Program.cs
+++ ForumApp/ForumApp/Program.cs
@@ -1,7 +1,18 @@
+using ForumApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ForumDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddControllersWithViews();
+
+
+
 
 var app = builder.Build();
 
