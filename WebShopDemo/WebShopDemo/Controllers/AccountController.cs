6b9810e9539381adc0fbb48905448956f766ffb4
CECO <cvetomir97@abv.bg>
2023-05-18 00:00:09 +0300

    Created AccountController and strating implementing methods.

---
+++ WebShopDemo/WebShopDemo/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebShopDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            return View();
+        }
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            return View();
+        }
+    }
+}
