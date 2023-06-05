aee773ecff3a10483d831f974bb0edbe169d62a0
CECO <cvetomir97@abv.bg>
2023-06-06 00:09:44 +0300

    Register,Login and Logout implementation.

--- ContactsApp/Contacts/Controllers/UserController.cs
+++ ContactsApp/Contacts/Controllers/UserController.cs
@@ -1,9 +1,101 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Contacts.Data.Entities;
+using Contacts.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Contacts.Controllers
 {
-    public class ApplicationUserController : Controller
+    [Authorize]
+    public class UserController : Controller
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Contacts");
+            }
+
+            var model = new RegisterViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Contacts");
+            }
+
+            var model = new LoginViewModel();
+            return View(model);
+
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await userManager.FindByNameAsync(model.UserName);
+            if (user != null)
+            {
+
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid Username or password, please try again.");
+            return View(model);
+
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
     }
 }
--- ContactsApp/Contacts/Models/LoginViewModel.cs
+++ ContactsApp/Contacts/Models/LoginViewModel.cs
@@ -2,25 +2,15 @@
 
 namespace Contacts.Models
 {
-    public class RegisterViewModel
+    public class LoginViewModel
     {
         [Required]
         [StringLength(20, MinimumLength = 5)]
         public string UserName { get; set; } = null!;
 
         [Required]
-        [StringLength(60, MinimumLength = 10)]
-        [EmailAddress]
-        public string Email { get; set; } = null!;
-
-        [Required]
         [DataType(DataType.Password)]
         [StringLength(20, MinimumLength = 5)]
         public string Password { get; set; } = null!;
-
-        [Required]
-        [DataType(DataType.Password)]
-        [Compare(nameof(Password))]
-        public string ConfirmPassword { get; set; } = null!;
     }
 }
